7e5892f4ff5e7b06e880d83cc22dd160486ae2e3
Thobias Zandisko Panjaitan <140685962+moonfaceisaac@users.noreply.github.com>
2025-03-13 12:51:58 +0700

    Fix all issues
    Updated README.md for a better set of instructions,
    updated both dashboard.py and notebook.ipynb to match
    the requirement of the project.

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 ## Step-by-step
 ```
 unzip files
-open and run notebook.ipynb
+open extracted directory with your editor
+select notebook.ipynb
+make sure all the required module have been installed, if not refer to requrirements.txt and install all the listed modules with : pip install "name of the module"
+run all the cells in .ipynb file
 ```
 
 ## Run steamlit app
 ```
-streamlit run dashboard.py
+streamlit run dashboard/dashboard.py
 ```
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,31 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 import seaborn as sns
 import streamlit as st
 
+st.title("Bike Rental Statistical Dashboard")
 
 colors = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
 
 time_qtr_df = pd.read_csv("dashboard/time_qtr.csv")
-all_time_df = pd.read_csv("dashboard/all_time.csv")
+season_time_df = pd.read_csv("dashboard/season_time.csv")
+timecyc_time_df = pd.read_csv("dashboard/timecyc_time.csv")
+wd_time_df = pd.read_csv("dashboard/wd_time.csv")
 
+
+with st.sidebar:
+    st.write("Welcome to Bike Rental Statistic Dashboard!")
 
 time_qtr_df['year'] = time_qtr_df['yearnquarter'].str.extract(r'(\d{4})').astype(int)
 time_qtr_df['quarter'] = time_qtr_df['yearnquarter'].str.extract(r'Q(\d)').astype(int)
 time_qtr_df = time_qtr_df.sort_values(by=['year', 'quarter'])
 
 
-st.subheader('Rental Bike Usage Trend')
+st.subheader('Rental Bike performance 2011-2012')
 
 col1, _ = st.columns(2)  
 
 with col1:
-    max_user = time_qtr_df["cnt_max"].max()
-    st.metric("Highest amount of rental user per year along with quarter", value=max_user)
+    max_user = round(time_qtr_df["cnt_mean"].max())
+    st.metric("Highest average in a quarter", value=max_user)
 
 
-fig, ax = plt.subplots(figsize=(16, 8))
+fig, ax = plt.subplots(figsize=(32, 16))
 ax.plot(
     time_qtr_df["yearnquarter"],
     time_qtr_df["cnt_mean"],
@@ -33,54 +40,63 @@ ax.plot(
     linewidth=2,
     color="#90CAF9"
 )
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
-ax.set_xlabel("Year & Quarter", fontsize=18)
-ax.set_ylabel("Average Rental Users", fontsize=18)
+ax.tick_params(axis='y', labelsize=35)
+ax.tick_params(axis='x', labelsize=40)
+ax.set_xlabel("Year & Quarter", fontsize=34)
+ax.set_ylabel("Average numbers of rental bike recorded", fontsize=34)
+ax.set_title("Rental Bike performance 2011-2012 based on average total recorded per quarter", loc="center",fontsize=50)
 
 st.pyplot(fig)
 
-st.subheader("Bike rental usage total")
-    
-col1, col2 = st.columns(2)
-    
-with col1:
-    fig, ax = plt.subplots(figsize=(20, 10))
-    
+
+x_val = " "
+st.subheader("Total Amount of Bike Rental recorded by time conditions")
+
+option = st.radio("Select Time Conditions:", ["By Season", "By Time Cycle","By Type of Day"])
+
+fig, ax = plt.subplots(figsize=(20, 10))
+
+if option == "By Season":
     sns.barplot(
-        y="total_customer", 
+        y="total_customer",
         x="season",
         hue="season",
-        data=all_time_df.sort_values(by="total_customer", ascending=False),
+        data=season_time_df.sort_values(by="total_customer", ascending=False),
         palette=colors,
         legend=False,
         ax=ax
     )
-    ax.set_title("Total amount Bike rental usage by season ", loc="center", fontsize=50)
-    ax.set_ylabel(None)
-    ax.set_xlabel(None)
-    ax.tick_params(axis='x', labelsize=35)
-    ax.tick_params(axis='y', labelsize=30)
-    st.pyplot(fig)
-    
-with col2:
-    fig, ax = plt.subplots(figsize=(20, 10))
-    
-
-    
+    ax.yaxis.set_major_formatter(mticker.StrMethodFormatter('{x:,.0f}'))
+    x_val = "Season"
+    ax.set_title("Total Amount of Bike Rental recorded based on season", loc="center", fontsize=50)
+elif option == "By Time Cycle":
     sns.barplot(
-        y="total_customer", 
+        y="total_customer",
         x="time_cycle",
         hue="time_cycle",
-        data=all_time_df.sort_values(by="total_customer", ascending=False),
+        data=timecyc_time_df.sort_values(by="total_customer", ascending=False),
         palette=colors,
         legend=False,
         ax=ax
     )
-    ax.set_title("Total Amount Bike rental usage by time cycle", loc="center", fontsize=50)
-    ax.set_ylabel(None)
-    ax.set_xlabel(None)
-    ax.tick_params(axis='x', labelsize=35)
-    ax.tick_params(axis='y', labelsize=30)
-    st.pyplot(fig)
-    
+    x_val = "Time Cycle"
+    ax.set_title("Total Amount of Bike Rental recorded based on time cycle", loc="center", fontsize=50)
+elif option == "By Type of Day":
+    sns.barplot(
+        y="total_customer",
+        x="workingday",
+        hue="workingday",
+        data=wd_time_df.sort_values(by="total_customer", ascending=False),
+        palette=colors,
+        legend=False,
+        ax=ax
+    )
+    ax.yaxis.set_major_formatter(mticker.StrMethodFormatter('{x:,.0f}'))
+    x_val = "Type of Day"
+    ax.set_title("Total Amount of Bike Rental recorded based on type of day", loc="center", fontsize=50)
+
+ax.set_ylabel("Total Amount of Bike Rental recorded",fontsize=28)
+ax.set_xlabel(x_val, fontsize=28)
+ax.tick_params(axis='x', labelsize=35)
+ax.tick_params(axis='y', labelsize=30)
+st.pyplot(fig)
